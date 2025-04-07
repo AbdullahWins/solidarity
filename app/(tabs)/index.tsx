44fcc5f1d8a18462a5aa73d141718ce05dcad56f
AbdullahWins <abdudevs@gmail.com>
2025-04-07 23:51:41 +0600

    feat: added logo to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { Image } from "react-native";
 import CustomBarcodeScanner from "../barcode-scanner";
 
 export default function HomeScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Solidarity" }} />
+      <Stack.Screen
+        options={{
+          title: "Solidarity",
+          headerRight: () => (
+            <Image
+              source={require("../../assets/images/icon.png")}
+              // resizeMode="contain"
+              accessibilityLabel="Solidarity Logo"
+              accessibilityRole="image"
+              style={{ width: 28, height: 28, marginRight: 10 }}
+            />
+          ),
+        }}
+      />
       <CustomBarcodeScanner />
     </>
   );
